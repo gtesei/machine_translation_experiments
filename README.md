6614367317b46d2d1ca349411ba61e5df315e06d
Ubuntu <ubuntu@ip-172-31-31-161.us-west-2.compute.internal>
2019-04-10 19:34:42 +0000

    ...

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # machine_translation_experiments
 Machine translation experiments
 
-## Tranfomers through fairseq
-  Fairseq(-py) is a sequence modeling toolkit from Facebook AI Research that allows researchers and developers to train custom models for translation, summarization, language modeling and other text generation tasks. 
-  URL: https://github.com/pytorch/fairseq
-
-### Requirements and Installation
+## Requirements and Installation
 
 * [PyTorch](http://pytorch.org/) version >= 1.0.0
 * Python version >= 3.6
@@ -21,6 +17,12 @@ After PyTorch is installed, you can install fairseq with `pip`:
 pip install fairseq
 ```
 
+Fairseq(-py) is a sequence modeling toolkit from Facebook AI Research that allows researchers and developers to train custom models for translation, summarization, language modeling and other text generation tasks.
+
+URL: https://github.com/pytorch/fairseq
+
+## [Tranfomers - Ott et al., 2018](https://arxiv.org/abs/1806.00187))
+
 ### Model 
   Description | Dataset | Model | Test set(s)
   ---|---|---|---
@@ -28,12 +30,29 @@ pip install fairseq
 
 ### Results 
   ```
-  $bash fairseq_wmt18_tranformer.sh
+  $bash fairseq_wmt18_tranformer_pre_wmt16.sh
   ...
   BLEU+case.lc+lang.en-de+numrefs.1+smooth.exp+test.wmt18+tok.13a+version.1.3.1 = 39.0 67.7/44.7/32.2/23.8 (BP = 1.000 ratio = 1.040 hyp_len = 66820 ref_len = 64276)
   Elapsed time (secs): 292
   ```
 
   The model achieves ~39 BLEU score and it needs ~292 seconds for predicting on the test of WMT18 (2,998 sentences). The state of the art (4/10/2019) for WMT18 ([Edunov et al., 2018](https://arxiv.org/abs/1808.09381); WMT'18 winner) claims 46.53 BLEU score although it is much slower.   
-  
+
+## [Convolutional - Gehring et al., 2017](https://arxiv.org/abs/1705.03122)
+
+### Model 
+  Description | Dataset | Model | Test set(s)
+  ---|---|---|---
+  Convolutional <br> ([Gehring et al., 2017](https://arxiv.org/abs/1705.03122)) | [WMT17 English-German](http://statmt.org/wmt17/translation-task.html#Download) | [download (.tar.bz2)](https://dl.fbaipublicfiles.com/fairseq/models/wmt17.v2.en-de.fconv-py.tar.bz2) | newstest2014: <br> [download (.tar.bz2)](https://dl.fbaipublicfiles.com/fairseq/data/wmt17.v2.en-de.newstest2014.tar.bz2)
+
+### Results 
+  ```
+  $bash fairseq_wmt18_tranformer_pre_wmt14.sh
+  ...
+  BLEU+case.lc+lang.en-de+numrefs.1+smooth.exp+test.wmt18+tok.13a+version.1.3.1 = 37.1 66.5/42.8/30.3/22.0 (BP = 1.000 ratio = 1.021 hyp_len = 65616 ref_len = 64276)
+  Elapsed time (secs): 221
+  ```
+
+  The model achieves ~37 BLEU score and it needs ~221 seconds for predicting on the test of WMT18 (2,998 sentences). The state of the art (4/10/2019) for WMT18 ([Edunov et al., 2018](https://arxiv.org/abs/1808.09381); WMT'18 winner) claims 46.53 BLEU score although it is much slower.   
+
 
---
+++ fairseq_wmt18_tranformer_pre_wmt14.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# First install sacrebleu and sentencepiece
+pip install sacrebleu sentencepiece
+
+### some checks 
+echo ">>> checking sacrebleu ..."
+
+SRC=en
+DEST=de
+CORPUS=wmt16
+
+echo ">> sample of src:"
+sacrebleu --test-set ${CORPUS} --language-pair ${SRC}-${DEST} --echo src | head
+
+echo ">> sample of ref:"
+sacrebleu --test-set ${CORPUS} --language-pair ${SRC}-${DEST} --echo ref | head
+
+echo ">> if we use ref we should have 100.0 BLUE score:"
+sacrebleu --test-set ${CORPUS} --language-pair ${SRC}-${DEST} --echo ref | sacrebleu --test-set ${CORPUS} --language-pair ${SRC}-${DEST}
+# BLEU+case.mixed+lang.en-de+numrefs.1+smooth.exp+test.wmt16+tok.13a+version.1.3.1 = 100.0 100.0/100.0/100.0/100.0 (BP = 1.000 ratio = 1.000 hyp_len = 62669 ref_len = 62669)
+####
+
+##############
+SRC=en
+DEST=de
+CORPUS=wmt18
+MODEL=wmt14.en-de.fconv-py
+CODE=code_wmt18
+
+mkdir -p data-bin
+if [ ! -d "data-bin/${MODEL}" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    curl https://dl.fbaipublicfiles.com/fairseq/models/wmt17.v2.en-de.fconv-py.tar.bz2 | tar xvjf - -C data-bin
+else
+    echo ">>> looks like you have already downloaded the model ..."
+fi
+#curl https://dl.fbaipublicfiles.com/fairseq/models/wmt16.en-de.joined-dict.transformer.tar.bz2 | tar xvjf - -C data-bin
+#curl https://dl.fbaipublicfiles.com/fairseq/models/wmt18.en-de.ensemble.tar.bz2 | tar xvjf - -C data-bin
+
+########### wmt18
+if [ ! -d "mosesdecoder" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    echo 'Cloning Moses github repository (for tokenization scripts)...'
+    git clone https://github.com/moses-smt/mosesdecoder.git
+else
+    echo ">>> looks like you have already downloaded  https://github.com/moses-smt/mosesdecoder.git  ..."
+fi
+
+if [ ! -d "mosesdecoder" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+    git clone https://github.com/rsennrich/subword-nmt.git
+else
+    echo ">>> looks like you have already downloaded  https://github.com/rsennrich/subword-nmt.git  ..."
+fi
+
+SCRIPTS=mosesdecoder/scripts
+TOKENIZER=$SCRIPTS/tokenizer/tokenizer.perl
+DETOKENIZER=$SCRIPTS/tokenizer/detokenizer.perl
+CLEAN=$SCRIPTS/training/clean-corpus-n.perl
+NORM_PUNC=$SCRIPTS/tokenizer/normalize-punctuation.perl
+REM_NON_PRINT_CHAR=$SCRIPTS/tokenizer/remove-non-printing-char.perl
+BPEROOT=subword-nmt/subword_nmt
+
+sacrebleu -t wmt18 -l en-de --echo src > wmt18.en-de.en
+START_TIME=$SECONDS
+cat wmt18.en-de.en| $NORM_PUNC -l en | $TOKENIZER -a -l en -q | python $BPEROOT/apply_bpe.py -c ${CODE} | fairseq-interactive data-bin/${MODEL} --path data-bin/${MODEL}/model.pt --remove-bpe --buffer-size 1024 --batch-size 16 -s ${SRC} -t ${DEST} | grep -P "^H" |cut -f 3- | $DETOKENIZER -l ${DEST} -q > ${CORPUS}.test.${SRC}-${DEST}.${SRC}.out
+
+cat ${CORPUS}.test.${SRC}-${DEST}.${SRC}.out | sacrebleu -t ${CORPUS} -l ${SRC}-${DEST} -lc
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Elapsed time (secs): $ELAPSED_TIME"
